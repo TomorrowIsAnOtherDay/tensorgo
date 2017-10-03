c2429a024b057ccf3ff3dba19212857170d3689e
TomorrowIsAnOtherDay <2466956298@qq.com>
2017-10-03 21:07:51 +0800

    add user-defined api for interaction

--- tensorgo/tfutils/gradient.py
+++ tensorgo/tfutils/gradient.py
@@ -27,6 +27,22 @@ def update_variable(server_varlist, worker_varlist):
     updated_vals.append(updated_val)
   return updated_vals
 
+def sync_variable(server_varlist, worker_varlist):
+  """sync variable between server and worker
+  @param
+    server_varlist: variable list of server
+    woker_varlist: variable list of worker(which to be assigned)
+  @retrun
+    updated_val: operation to sync variables
+  """
+  assert len(worker_varlist) == len(server_varlist)
+  n_vars = len(worker_varlist)
+  updated_vals = []
+  for i in xrange(n_vars):
+    updated_val = worker_varlist[i].assign(server_varlist[i])
+    updated_vals.append(updated_val)
+  return updated_vals
+
 def compute_worker_gradient(server_varlist, worker_varlist):
   assert len(server_varlist) == len(worker_varlist)
   n_vars = len(server_varlist)
@@ -44,3 +60,30 @@ def apply_worker_gradients(opt, server_param, delta, scale=1.0):
     var_grads.append((delta[i], server_param[i]))
   apply_gradients = opt.apply_gradients(var_grads)
   return apply_gradients
+
+fetch_cache = dict()
+def fetch_all_vars(workers_varlist, fetches, n_towers):
+  """fetch all same variables/operations in all workers
+  Args:
+      workers_varlist: worker variables list 
+      fetches: specific which var/op to fetch
+  """
+  all_worker_fetches = []
+  for ele in fetches:
+    ele_name = ele.name
+    assert 'tower' in ele.name
+
+    if ele_name in fetch_cache:
+      cur_ele_fetches = fetch_cache[ele_name]
+    else:
+      cur_ele_fetches = []
+      for i in range(n_towers):
+        cur_tower_str = 'tower_{}'.format(n_towers - 1)
+        target_tower_str = 'tower_{}'.format(i)
+        target_ele_name = ele_name.replace(cur_tower_str, target_tower_str)
+        tartget_ele = tf.get_default_graph().get_tensor_by_name(target_ele_name)
+        cur_ele_fetches.append(tartget_ele)
+      assert len(cur_ele_fetches) == n_towers
+      fetch_cache[ele_name] = cur_ele_fetches
+    all_worker_fetches.append(cur_ele_fetches)
+  return all_worker_fetches
--- tensorgo/train/multigpu.py
+++ tensorgo/train/multigpu.py
@@ -7,7 +7,8 @@ import numpy as np
 import tensorflow as tf
 from tensorgo.utils import logger
 from tensorgo.train.config import TrainConfig
-from tensorgo.tfutils.gradient import record_variable, update_variable, compute_worker_gradient, apply_worker_gradients
+from tensorgo.tfutils.gradient import record_variable, update_variable, \
+    compute_worker_gradient, apply_worker_gradients, sync_variable, fetch_all_vars
 import time
 import threading
 
@@ -25,14 +26,10 @@ class MultiGpuTrainer(object):
 
     self._work_train_op = []
     self._server_param = []
+    self._worker_param = []
     self._deltas = []
     self._wrap_delta = []
     self._sync_worker = []
-
-    # specific
-    self._probs = []
-    self._labels = []
-    # end
 
     self._setup_inputs()
     self._setup()
@@ -56,39 +53,65 @@ class MultiGpuTrainer(object):
         logger.info('buildding tower:{}'.format(i))
         with tf.device('/gpu:{}'.format(i)):
           with tf.variable_scope('tower_{}'.format(i)):
-            loss, prob, label = self._model.build_graph(self._data_iter)
-            self._probs.append(prob)
-            self._labels.append(label)
-            #tf.get_variable_scope().reuse_variables()
+            loss = self._model.build_graph(self._data_iter)
             varlist = tf.trainable_variables()
             val_keyword = 'tower_{}'.format(i)
             varlist = filter(lambda x: val_keyword in x.name, varlist)
-            # recorde server baseline model
+            self._worker_param.append(varlist)
+
+            # record server model
             if i == 0:
-              self._server_param = record_variable(varlist)
+              with tf.device('/cpu:0'):
+                self._server_param = record_variable(varlist)
+
             #push & pull
             sync_worker = update_variable(self._server_param, varlist)
-            delta = compute_worker_gradient(self._server_param, varlist)
-            wrap_delta = apply_worker_gradients(opt, self._server_param, delta, scale=(1.0 / self._config.n_towers) * 0.5)
+            with tf.device('/cpu:0'):
+              delta = compute_worker_gradient(self._server_param, varlist)
+              wrap_delta = apply_worker_gradients(opt, self._server_param, delta, scale=(1.0 / self._config.n_towers) * 0.5)
             self._sync_worker.extend(sync_worker)
             self._deltas.extend(delta)
             self._wrap_delta.append(wrap_delta)
             # end
             worker_train_op = opt.minimize(loss)
             self._work_train_op.append(worker_train_op)
+
+    # init session
     self._sess = tf.Session(config=tf.ConfigProto(
       log_device_placement=False,
       allow_soft_placement=True,
       gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=1.0, allow_growth=True)
         ))
-    init = tf.global_variables_initializer()
-    self._sess.run(init)
+    
+    # init graph
+    self.init_graph()
+
+    # init input
     self._sess.run(self._init_data_op)
     self._adam_varlist = filter(lambda var: 'Adam' not in var.name, tf.global_variables())
 
+  def init_graph(self):
+    """init woker and server variable, all the variable should be same after initialization"""
+    logger.info("Initializing global parameters")
+    init = tf.global_variables_initializer()
+    self._sess.run(init)
+
+    # used first woker parameter as global initial parameter
+    return 
+    for i in range(1, self._config.n_towers):
+      sync_op = sync_variable(self._worker_param[0], self._worker_param[i])
+      self._sess.run(sync_op)
+    sync_server_op = sync_variable(self._worker_param[0], self._server_param)
+    self._sess.run(sync_server_op)
+    logger.info("Global parameters, all parameter are same")
+
   def push_and_pull(self):
+    """aggregate workers' gradient to server and sync the model of workers"""
+
+    # unnecessary to sync model when there is only one worker
     if self._config.n_towers == 1:
       return 
+
     start = time.time()
     wrap_gradients = self._deltas + self._wrap_delta
     #self._sess.run(self._deltas)
@@ -104,8 +127,14 @@ class MultiGpuTrainer(object):
     op_list = [var.assign(var.initialized_value()) for var in self._adam_varlist]
     self._sess.run(op_list)
 
-  def run(self, feed_dict=None, test=False):
-    #if self._config.n_towers > 1 and self.batch_count % self._config.commbatch == 0:
+  def run(self, fetches=[], feed_dict=None, test=False, one_worker=False):
+    """
+    Args:
+        fetches: A single graph element, a list of graph elements
+        feed_dict:  A dictionary that maps graph elements to values
+        test: whether to run train op
+        one_worker: use only one worker(often used in evaluation, which could keep data's order )
+    """
     if self.batch_count % self._config.commbatch == 0 and not test:
       logger.info('[run] batch_count:{}  sync the worker'.format(self.batch_count))
       #self.push_and_pull()
@@ -113,15 +142,26 @@ class MultiGpuTrainer(object):
       t.start()
       logger.info('[run] batch_count:{}  sync the worker end'.format(self.batch_count))
 
+    if one_worker == False:
+      final_fetches = fetch_all_vars(self._worker_param, fetches, self._config.n_towers)
+    else: 
+      final_fetches = fetches
+
     if not test:
-      ret = self._sess.run(self._probs + self._labels + [self._work_train_op], feed_dict)
-      self.batch_count += 1
-      probs = ret[:self._config.n_towers]
-      labels = ret[self._config.n_towers: self._config.n_towers * 2]
+      final_fetches.append(self._work_train_op)
+    self.batch_count += 1
+
+    ret = self._sess.run(final_fetches, feed_dict=feed_dict)
+    # reorganize its output
+    if len(fetches) == 0:
+      return []
     else:
-      ret = self._sess.run([self._probs[0], self._labels[0]], feed_dict=feed_dict)
-      probs = ret[0]
-      labels = ret[1]
-    #logger.info("[run] batch_count:{}   commbatch:{}".format(self.batch_count, self._config.commbatch))
-    #logger.info('all_variables:{}'.format(tf.all_variables()))
-    return probs, labels
+      merge_ret = []
+      fetches_num = len(fetches)
+
+      for i in range(fetches_num):
+        if one_worker:
+          merge_ret.append(ret[i])
+        else:
+          merge_ret.append(np.concatenate(ret[i]))
+      return merge_ret
