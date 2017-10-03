ba363f9302fc6b6a67f94311d48a54604f4ef5b4
TomorrowIsAnOtherDay <2466956298@qq.com>
2017-10-03 10:12:29 +0800

    append comments for some functions

--- tensorgo/tfutils/gradient.py
+++ tensorgo/tfutils/gradient.py
@@ -6,11 +6,15 @@ import tensorflow as tf
 __all__ = ['record_variable']
 
 def record_variable(varlist):
+  """ record variable list as parameter server 
+    @param:
+      varlist: variable list which should sync among workers and servers
+  """
   record_vars = []
   for var in varlist:
     var_name = '_'.join(var.name.split('/')[-3:])
     var_name = var_name.replace(':', '_')
-    record_var = tf.get_variable('old_{}'.format(var_name), shape=var.get_shape(), trainable=False)
+    record_var = tf.get_variable('server_{}'.format(var_name), shape=var.get_shape(), trainable=False)
     record_vars.append(record_var)
   return record_vars
 
@@ -19,7 +23,7 @@ def update_variable(server_varlist, worker_varlist):
   n_vars = len(worker_varlist)
   updated_vals = []
   for i in xrange(n_vars):
-    updated_val = worker_varlist[i].assign(server_varlist[i])
+    updated_val = worker_varlist[i].assign_add(server_varlist[i] - worker_varlist[i])
     updated_vals.append(updated_val)
   return updated_vals
 
